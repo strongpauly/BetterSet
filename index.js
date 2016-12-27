14a82c28f128cf3a84ca5f842f102a4a430213d2
Paul Potsides <paul.potsides@devicepilot.com>
2016-12-27 14:25:53 +0000

    Changed to standard.js

--- index.js
+++ index.js
@@ -1,115 +1,112 @@
-'use strict';
+'use strict'
 
-/*jslint node: true */
+/* jslint node: true */
 
 /**
  * A set proxy that adds some useful methods that should (IMO) be part of the base spec.
  */
 module.exports = (function () {
-    class BetterSet extends Set {
+  class BetterSet extends Set {
 
-        constructor(iterable) {
-            super(iterable);
-        }
+    /**
+     * Add all items in an iterable to this set.
+     */
+    addAll (iterable) {
+      if (!iterable || typeof iterable.forEach !== 'function') {
+        return
+      }
+      iterable.forEach(item => {
+        this.add(item)
+      })
+    }
 
-        /**
-         * Add all items in an iterable to this set.
-         */
-        addAll (iterable) {
-            if (!iterable || typeof iterable.forEach !== 'function') {
-                return;
-            }
-            iterable.forEach(item => {
-                this.add(item);
-            });
-        }
-        /**
-         * Deletes all items from this set.
-         */
-        deleteAll (iterable) {
-            if (!iterable || typeof iterable.forEach !== 'function') {
-                return;
-            }
-            iterable.forEach(item => {
-                this.delete(item);
-            });
-        }
+    /**
+     * Deletes all items from this set.
+     */
+    deleteAll (iterable) {
+      if (!iterable || typeof iterable.forEach !== 'function') {
+        return
+      }
+      iterable.forEach(item => {
+        this.delete(item)
+      })
+    }
 
-        /**
-         * Returns a union of this set with another Set or iterable.
-         */
-        union (otherSet) {
-            var newSet = new BetterSet();
-            newSet.addAll(this);
-            newSet.addAll(otherSet);
-            return newSet;
-        }
+    /**
+     * Returns a union of this set with another Set or iterable.
+     */
+    union (otherSet) {
+      let newSet = new BetterSet()
+      newSet.addAll(this)
+      newSet.addAll(otherSet)
+      return newSet
+    }
 
         /**
          * Returns an intersection of this set with another Set.
          */
-        intersection (otherSet) {
-            var newSet = new BetterSet();
-            this.forEach( item => {
-                if (otherSet.has(item)) {
-                    newSet.add(item);
-                }
-            });
-            return newSet;
+    intersection (otherSet) {
+      let newSet = new BetterSet()
+      this.forEach(item => {
+        if (otherSet.has(item)) {
+          newSet.add(item)
         }
-
-        /**
-         * Returns a relative complement of this set with another,
-         * i.e. all the items that are in this set, but not in the other.
-         */
-        complement (otherSet) {
-            var newSet = new BetterSet();
-            this.forEach( item => {
-                if (!otherSet.has(item)) {
-                    newSet.add(item);
-                }
-            });
-            return newSet;
-        }
-
-        /**
-         * Returns a symmetric difference of the two sets,
-         * i.e. all the items that were not in both sets.
-         * Can be thought of as the negative intersection.
-         */
-        difference (otherSet) {
-            var newSet = new BetterSet();
-            //Union two sets first.
-            var union = this.union(otherSet);
-            union.forEach( item => {
-                //Only add it to the new set if it doesn't exist in both.
-                if (!(otherSet.has(item) && this.has(item))) {
-                    newSet.add(item);
-                }
-            });
-            return newSet;
-        }
-
-        /**
-         * Convenience function turning this into an array.
-         */
-        asArray () {
-            return Array.from(this);
-        }
-
-        /**
-         * Convenience function acting like Array.prototype.map.
-         */
-        map (mapFunction, thisArg) {
-            return new BetterSet(this.asArray().map(mapFunction, thisArg));
-        }
-
-        /**
-         * Convenience function acting like Array.prototype.reduce.
-         */
-        reduce (reduceFunction, initialValue) {
-            return this.asArray().reduce(reduceFunction, initialValue);
-        }
+      })
+      return newSet
     }
-    return BetterSet;
-}());
+
+    /**
+     * Returns a relative complement of this set with another,
+     * i.e. all the items that are in this set, but not in the other.
+     */
+    complement (otherSet) {
+      let newSet = new BetterSet()
+      this.forEach(item => {
+        if (!otherSet.has(item)) {
+          newSet.add(item)
+        }
+      })
+      return newSet
+    }
+
+    /**
+     * Returns a symmetric difference of the two sets,
+     * i.e. all the items that were not in both sets.
+     * Can be thought of as the negative intersection.
+     */
+    difference (otherSet) {
+      let newSet = new BetterSet()
+      // Union two sets first.
+      let union = this.union(otherSet)
+      union.forEach(item => {
+        // Only add it to the new set if it doesn't exist in both.
+        if (!(otherSet.has(item) && this.has(item))) {
+          newSet.add(item)
+        }
+      })
+      return newSet
+    }
+
+    /**
+     * Convenience function turning this into an array.
+     */
+    asArray () {
+      return Array.from(this)
+    }
+
+    /**
+     * Convenience function acting like Array.prototype.map.
+     */
+    map (mapFunction, thisArg) {
+      return new BetterSet(this.asArray().map(mapFunction, thisArg))
+    }
+
+    /**
+     * Convenience function acting like Array.prototype.reduce.
+     */
+    reduce (reduceFunction, initialValue) {
+      return this.asArray().reduce(reduceFunction, initialValue)
+    }
+  }
+  return BetterSet
+}())
--- test/index.js
+++ test/index.js
@@ -1,183 +1,183 @@
-'use strict';
-/*eslint-env node, mocha */
-/*jslint node: true */
+'use strict'
+/* eslint-env node, mocha */
+/* jslint node: true */
 
 // Chai
-var chai = require("chai");
+let chai = require('chai')
 
 // Chai Promises
-var chaiAsPromised = require("chai-as-promised");
+let chaiAsPromised = require('chai-as-promised')
 
 // Get utilities.
-var Set = require('../index.js');
+let Set = require('../index.js')
 
-chai.use(chaiAsPromised);
-chai.should();
+chai.use(chaiAsPromised)
+chai.should()
 
-describe('Set', function(){
-    it('should allow items to be added', () => {
-        var theSet = new Set();
-        theSet.add(1);
-        theSet.has(1).should.equal(true);
-        theSet.size.should.equal(1);
-    });
+describe('Set', function () {
+  it('should allow items to be added', () => {
+    let theSet = new Set()
+    theSet.add(1)
+    theSet.has(1).should.equal(true)
+    theSet.size.should.equal(1)
+  })
 
-    it('should allow items to be deleted', () => {
-        var theSet = new Set();
-        theSet.add(1);
-        theSet.has(1).should.equal(true);
-        theSet.size.should.equal(1);
-        theSet.delete(1);
-        theSet.has(1).should.equal(false);
-        theSet.size.should.equal(0);
-    });
+  it('should allow items to be deleted', () => {
+    let theSet = new Set()
+    theSet.add(1)
+    theSet.has(1).should.equal(true)
+    theSet.size.should.equal(1)
+    theSet.delete(1)
+    theSet.has(1).should.equal(false)
+    theSet.size.should.equal(0)
+  })
 
-    it('should be clearable', () => {
-        var theSet = new Set();
-        theSet.add(1);
-        theSet.has(1).should.equal(true);
-        theSet.size.should.equal(1);
-        theSet.clear();
-        theSet.has(1).should.equal(false);
-        theSet.size.should.equal(0);
-    });
+  it('should be clearable', () => {
+    let theSet = new Set()
+    theSet.add(1)
+    theSet.has(1).should.equal(true)
+    theSet.size.should.equal(1)
+    theSet.clear()
+    theSet.has(1).should.equal(false)
+    theSet.size.should.equal(0)
+  })
 
-    it('items should be iterable via entries function', () => {
-        var mySet = new Set();
-        mySet.add("foobar");
-        mySet.add(1);
-        mySet.add("baz");
+  it('items should be iterable via entries function', () => {
+    let mySet = new Set()
+    mySet.add('foobar')
+    mySet.add(1)
+    mySet.add('baz')
 
-        let setIter = mySet.entries();
-        setIter.next().value.should.eql(["foobar", "foobar"]);
-        setIter.next().value.should.eql([1, 1]);
-        setIter.next().value.should.eql(["baz", "baz"]);
-    });
+    let setIter = mySet.entries()
+    setIter.next().value.should.eql(['foobar', 'foobar'])
+    setIter.next().value.should.eql([1, 1])
+    setIter.next().value.should.eql(['baz', 'baz'])
+  })
 
-    it('items should be iterable via values function', () => {
-        var mySet = new Set();
-        mySet.add("foobar");
-        mySet.add(1);
-        mySet.add("baz");
+  it('items should be iterable via values function', () => {
+    let mySet = new Set()
+    mySet.add('foobar')
+    mySet.add(1)
+    mySet.add('baz')
 
-        let setIter = mySet.values();
-        setIter.next().value.should.eql("foobar");
-        setIter.next().value.should.eql(1);
-        setIter.next().value.should.eql("baz");
-    });
+    let setIter = mySet.values()
+    setIter.next().value.should.eql('foobar')
+    setIter.next().value.should.eql(1)
+    setIter.next().value.should.eql('baz')
+  })
 
-    it('items should be iterable via keys function', () => {
-        var mySet = new Set();
-        mySet.add("foobar");
-        mySet.add(1);
-        mySet.add("baz");
+  it('items should be iterable via keys function', () => {
+    let mySet = new Set()
+    mySet.add('foobar')
+    mySet.add(1)
+    mySet.add('baz')
 
-        let setIter = mySet.keys();
-        setIter.next().value.should.eql("foobar");
-        setIter.next().value.should.eql(1);
-        setIter.next().value.should.eql("baz");
-    });
+    let setIter = mySet.keys()
+    setIter.next().value.should.eql('foobar')
+    setIter.next().value.should.eql(1)
+    setIter.next().value.should.eql('baz')
+  })
 
-    it('items should be iterable via forEach function', (done) => {
-        var sawFoo = false,
-            sawBar = false,
-            sawUndefined = false;
-        new Set(["foo", "bar", undefined]).forEach( (value) => {
-            if (value === 'foo') {
-                sawFoo.should.equal(false);
-                sawFoo = true;
-            } else if (value === 'bar') {
-                sawBar.should.equal(false);
-                sawBar = true;
-            } else if (value === undefined) {
-                sawUndefined.should.equal(false);
-                sawUndefined = true;
-            } else {
-                done('forEach called with unknown value: ' + value);
-            }
-        });
-        sawFoo.should.equal(true);
-        sawBar.should.equal(true);
-        sawUndefined.should.equal(true);
-        done();
-    });
+  it('items should be iterable via forEach function', (done) => {
+    let sawFoo = false
+    let sawBar = false
+    let sawUndefined = false
+    new Set(['foo', 'bar', undefined]).forEach(value => {
+      if (value === 'foo') {
+        sawFoo.should.equal(false)
+        sawFoo = true
+      } else if (value === 'bar') {
+        sawBar.should.equal(false)
+        sawBar = true
+      } else if (value === undefined) {
+        sawUndefined.should.equal(false)
+        sawUndefined = true
+      } else {
+        done('forEach called with unknown value: ' + value)
+      }
+    })
+    sawFoo.should.equal(true)
+    sawBar.should.equal(true)
+    sawUndefined.should.equal(true)
+    done()
+  })
 
-    it('should be able to be unioned', () => {
-        var setA = new Set([1, 2, 3]),
-            setB = new Set([4, 5, 6]),
-            setC = setA.union(setB);
-        let setIter = setC.keys();
-        setIter.next().value.should.eql(1);
-        setIter.next().value.should.eql(2);
-        setIter.next().value.should.eql(3);
-        setIter.next().value.should.eql(4);
-        setIter.next().value.should.eql(5);
-        setIter.next().value.should.eql(6);
-    });
+  it('should be able to be unioned', () => {
+    let setA = new Set([1, 2, 3])
+    let setB = new Set([4, 5, 6])
+    let setC = setA.union(setB)
+    let setIter = setC.keys()
+    setIter.next().value.should.eql(1)
+    setIter.next().value.should.eql(2)
+    setIter.next().value.should.eql(3)
+    setIter.next().value.should.eql(4)
+    setIter.next().value.should.eql(5)
+    setIter.next().value.should.eql(6)
+  })
 
-    it('should be able to be intersectioned', () => {
-        var setA = new Set([1, 2, 3, 4, 5]),
-            setB = new Set([4, 5, 6]),
-            setC = setA.intersection(setB);
-        let setIter = setC.keys();
-        setIter.next().value.should.eql(4);
-        setIter.next().value.should.eql(5);
-        setA.size.should.equal(5);
-        setB.size.should.equal(3);
-        setC.size.should.equal(2);
-    });
+  it('should be able to be intersectioned', () => {
+    let setA = new Set([1, 2, 3, 4, 5])
+    let setB = new Set([4, 5, 6])
+    let setC = setA.intersection(setB)
+    let setIter = setC.keys()
+    setIter.next().value.should.eql(4)
+    setIter.next().value.should.eql(5)
+    setA.size.should.equal(5)
+    setB.size.should.equal(3)
+    setC.size.should.equal(2)
+  })
 
-    it('should be able to be complemented', () => {
-        var setA = new Set([1, 2, 3, 4, 5]),
-            setB = new Set([4, 5, 6]),
-            setC = setA.complement(setB);
-        let setIter = setC.keys();
-        setIter.next().value.should.eql(1);
-        setIter.next().value.should.eql(2);
-        setIter.next().value.should.eql(3);
-        setA.size.should.equal(5);
-        setB.size.should.equal(3);
-        setC.size.should.equal(3);
-    });
+  it('should be able to be complemented', () => {
+    let setA = new Set([1, 2, 3, 4, 5])
+    let setB = new Set([4, 5, 6])
+    let setC = setA.complement(setB)
+    let setIter = setC.keys()
+    setIter.next().value.should.eql(1)
+    setIter.next().value.should.eql(2)
+    setIter.next().value.should.eql(3)
+    setA.size.should.equal(5)
+    setB.size.should.equal(3)
+    setC.size.should.equal(3)
+  })
 
-    it('should be able to be differenced', () => {
-        var setA = new Set([1, 2, 3, 4, 5]),
-            setB = new Set([4, 5, 6]),
-            setC = setA.difference(setB);
-        let setIter = setC.keys();
-        setIter.next().value.should.eql(1);
-        setIter.next().value.should.eql(2);
-        setIter.next().value.should.eql(3);
-        setIter.next().value.should.eql(6);
-        setA.size.should.equal(5);
-        setB.size.should.equal(3);
-        setC.size.should.equal(4);
-    });
+  it('should be able to be differenced', () => {
+    let setA = new Set([1, 2, 3, 4, 5])
+    let setB = new Set([4, 5, 6])
+    let setC = setA.difference(setB)
+    let setIter = setC.keys()
+    setIter.next().value.should.eql(1)
+    setIter.next().value.should.eql(2)
+    setIter.next().value.should.eql(3)
+    setIter.next().value.should.eql(6)
+    setA.size.should.equal(5)
+    setB.size.should.equal(3)
+    setC.size.should.equal(4)
+  })
 
-    it('should receive all items from addAll', () => {
-        var set = new Set([1, 2, 3]);
-        set.addAll([4, 5, 6]);
-        let setIter = set.values();
-        setIter.next().value.should.eql(1);
-        setIter.next().value.should.eql(2);
-        setIter.next().value.should.eql(3);
-        setIter.next().value.should.eql(4);
-        setIter.next().value.should.eql(5);
-        setIter.next().value.should.eql(6);
-    });
+  it('should receive all items from addAll', () => {
+    let set = new Set([1, 2, 3])
+    set.addAll([4, 5, 6])
+    let setIter = set.values()
+    setIter.next().value.should.eql(1)
+    setIter.next().value.should.eql(2)
+    setIter.next().value.should.eql(3)
+    setIter.next().value.should.eql(4)
+    setIter.next().value.should.eql(5)
+    setIter.next().value.should.eql(6)
+  })
 
-    it('should be mappable into another set.', () => {
-        var set = new Set([1, 2, 3]);
-        var newSet = set.map(item => item * 2);
-        let setIter = newSet.values();
-        setIter.next().value.should.eql(2);
-        setIter.next().value.should.eql(4);
-        setIter.next().value.should.eql(6);
-    });
+  it('should be mappable into another set.', () => {
+    let set = new Set([1, 2, 3])
+    let newSet = set.map(item => item * 2)
+    let setIter = newSet.values()
+    setIter.next().value.should.eql(2)
+    setIter.next().value.should.eql(4)
+    setIter.next().value.should.eql(6)
+  })
 
-    it('should be reduceable.', () => {
-        var set = new Set([1, 2, 3]);
-        var result = set.reduce((lastItem, item) => lastItem + item, 0);
-        result.should.equal(6);
-    });
-});
+  it('should be reduceable.', () => {
+    let set = new Set([1, 2, 3])
+    let result = set.reduce((lastItem, item) => lastItem + item, 0)
+    result.should.equal(6)
+  })
+})
