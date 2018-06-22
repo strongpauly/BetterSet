95ef4abc8203bb4fae43169fea4f0326b28b1d4f
Paul Potsides <strongpauly@hotmail.com>
2018-06-22 11:15:15 +0100

    Add types.

---
+++ src/main.d.ts
@@ -0,0 +1,13 @@
+
+declare class BetterSet<T> extends Set<T> {
+    addAll: (iterable:Iterable<T>) => void;
+    deleteAll: () => void;
+    union(otherSet:Set<T>): BetterSet<T>
+    intersection(otherSet:Set<T>): BetterSet<T>
+    complement(otherSet:Set<T>): BetterSet<T>
+    difference(otherSet:Set<T>): BetterSet<T>
+    equals(otherSet:Set<T>):boolean;
+    asArray():T[];
+    map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): BetterSet<U>
+    reduce<U>(callbackfn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U, initialValue: U): U;
+}
